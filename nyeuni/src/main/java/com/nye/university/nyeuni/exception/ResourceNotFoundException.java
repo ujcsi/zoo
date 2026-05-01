e704b471bc0e84a7360dfaf6557af4c856067780
ujcsi <ujcsi@satrax.hu>
2026-05-01 19:06:29 +0200

    GondozoService

--- nyeuni/src/main/java/com/nye/university/nyeuni/exception/ResourceNotFoundException.java
+++ nyeuni/src/main/java/com/nye/university/nyeuni/exception/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package com.nye.university.nyeuni.exception;
 
-public class ResourceNotFoundException extends Exception {
+public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
         super(message);
--- nyeuni/src/main/java/com/nye/university/nyeuni/service/GondozoService.java
+++ nyeuni/src/main/java/com/nye/university/nyeuni/service/GondozoService.java
@@ -1,4 +1,76 @@
 package com.nye.university.nyeuni.service;
 
+
+import com.nye.university.nyeuni.dto.gondozo.GondozoRequestDto;
+import com.nye.university.nyeuni.dto.gondozo.GondozoResponseDto;
+import com.nye.university.nyeuni.entity.Gondozo;
+import com.nye.university.nyeuni.exception.ResourceNotFoundException;
+import com.nye.university.nyeuni.repository.GondozoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
 public class GondozoService {
+
+    @Autowired
+    private GondozoRepository gondozoRepository;
+
+    public GondozoResponseDto createGondozo (GondozoRequestDto gondozoRequestDto) {
+        Gondozo gondozo = Gondozo.builder()
+                .terulet(gondozoRequestDto.getTerulet())
+                .gondozonev(gondozoRequestDto.getGondozonev())
+                .build();
+        Gondozo savedGondozo =gondozoRepository.save(gondozo);
+        return mapToDto(savedGondozo);
+    }
+
+    public List<GondozoResponseDto> getAllGondozos () {
+        return gondozoRepository.findAll()
+            .stream()
+            .map(this::mapToDto)
+            .toList();
+    }
+
+    public GondozoResponseDto getGondozoById (Long id) {
+        Optional<Gondozo> gondozoOptional = gondozoRepository.findById(id);
+        Gondozo gondozo = gondozoOptional
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("Nincs ilyen gondozo id: "+ id )
+                );
+        return mapToDto(gondozo);
+    }
+
+
+
+    public GondozoResponseDto updateGondozo(Long id, GondozoRequestDto requestDto) {
+        Gondozo gondozo = gondozoRepository.findById(id)
+                .orElseThrow(
+                ()-> new ResourceNotFoundException("Nincs ilyen gondozo id: " + id)
+                );
+
+        gondozo.setTerulet(requestDto.getTerulet());
+        gondozo.setGondozonev(requestDto.getGondozonev());
+
+        Gondozo updatedGondozo = gondozoRepository.save(gondozo);
+        return mapToDto(updatedGondozo);
+    }
+
+    public void deleteGondozo (Long id) {
+        gondozoRepository.deleteById(id);
+    }
+
+    private GondozoResponseDto mapToDto(Gondozo gondozo) {
+        return GondozoResponseDto.builder()
+                .id(gondozo.getId())
+                .terulet(gondozo.getTerulet())
+                .gondozonev(gondozo.getGondozonev())
+                .build();
+    }
+
 }
+
+
