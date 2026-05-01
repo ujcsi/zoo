5c8efb339c9f9c65b0ba79f28e55909d8fef2913
ujcsi <ujcsi@satrax.hu>
2026-05-01 09:57:05 +0200

    Alapbeállítások

---
+++ nyeuni/src/main/java/com/nye/university/nyeuni/NyeuniApplication.java
@@ -0,0 +1,13 @@
+package com.nye.university.nyeuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyeuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyeuniApplication.class, args);
+	}
+
+}
---
+++ nyeuni/src/test/java/com/nye/university/nyeuni/NyeuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nye.university.nyeuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyeuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
